2a01e2d40494b7edca2ae6271188fd26c3b85516
Noken29 <42448187+Noken29@users.noreply.github.com>
2021-05-15 14:25:24 +0300

    Deleted comments

--- src/main/resources/app/index.js
+++ src/main/resources/app/index.js
@@ -12,14 +12,7 @@ if(a.value !== '' && b.value !== '' && b.value !== ''){
     var data = JSON.stringify({'a': a.value, 'b': b.value, 'c': c.value, 'result': result})
     console.log(data);
     xhr.send(data);
-    /*$.ajax({
-        url: 'http://localhost:8080/api/save',
-        type: 'POST',
-        data: JSON.stringify({'a': a.value, 'b': b.value, 'c': c.value, 'result': '123'}),
-        contentType: 'application/json; charset=utf-8',
-        dataType: 'json',
-        async: false,
-    });*/
+    
     }
     else
         document.getElementById('result').textContent = 'Coefficients must be not null'
